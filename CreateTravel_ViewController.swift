e8f7ed2f8c06164fb0ba80d72ff887bdb0b41c11
Ines MISSOUM-BENZIANE <ines.missoum-benzian@igtp3-mac10.isim.intra>
2019-03-26 19:31:40 +0100

    Update creation travel

--- CreateTravel_ViewController.swift
+++ CreateTravel_ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class CreateTravel_ViewController: UIViewController {
 
+    var newTravel : Travel?
     
     @IBOutlet weak var title_field: UITextField!
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var startDate: UIDatePicker!
     @IBOutlet weak var endDate: UIDatePicker!
     
+    // preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "CreateTravel" {
+            let title : String  = self.title_field.text!
+            self.newTravel = Travel(t: title)
+        }
+        else{
+            self.newTravel = nil
+        } }
+    
+    
+    // MARK: - TextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text{
+            if text != ""{
+                textField.resignFirstResponder()
+                return true
+            } }
+        return false
+    }
+
     
 }
