4e1086f54bf4d02b24d834a6671be8592eca0a32
Ines MISSOUM-BENZIANE <ines.missoum-benzian@igtp3-mac10.isim.intra>
2019-03-26 16:40:47 +0100

    files for CRUD Travel created

---
+++ ManageUrTrip/CoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataManager.swift
+//  ManageUrTrip
+//
+//  Created by Ines MISSOUM-BENZIANE on 26/03/2019.
+//  Copyright © 2019 Marine GARDEISEN. All rights reserved.
+//
+
+import Foundation
---
+++ Travel_TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  Travel_TableViewController.swift
+//  ManageUrTrip
+//
+//  Created by Ines MISSOUM-BENZIANE on 26/03/2019.
+//  Copyright © 2019 Marine GARDEISEN. All rights reserved.
+//
+
+import UIKit
+
+class Travel_TableViewController: NSObject {
+
+}
