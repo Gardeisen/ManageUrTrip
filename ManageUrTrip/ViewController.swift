00d58bd56a6e0d699977a0cdd232d0292bdf2d00
Marine GARDEISEN <marine.gardeisen@igtp3-mac09.isim.intra>
2019-03-26 10:40:07 +0100

    first commit

---
+++ ManageUrTrip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManageUrTrip
+//
+//  Created by Marine GARDEISEN on 26/03/2019.
+//  Copyright © 2019 Marine GARDEISEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
