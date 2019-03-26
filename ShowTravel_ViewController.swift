7f569b14ae975bd70af6f61176b7043b2a2560d9
Ines MISSOUM-BENZIANE <ines.missoum-benzian@igtp3-mac10.isim.intra>
2019-03-26 19:16:59 +0100

    Update

--- ShowTravel_ViewController.swift
+++ ShowTravel_ViewController.swift
@@ -23,6 +23,9 @@ class ShowTravel_ViewController: UIViewController {
         if let atravel = self.travelSelected {
             self.name.text = atravel.title
             self.name.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            self.image.image = UIImage(named: "beach")
+            
+            
             
             
             
