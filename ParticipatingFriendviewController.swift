0a09830f66ccc9f33c54babced9b46b02475dece
Marine GARDEISEN <marine.gardeisen@igtp4-mac09.isim.intra>
2019-03-27 19:18:44 +0100

    update entité Friend

---
+++ ParticipatingFriendviewController.swift
@@ -0,0 +1,28 @@
+//
+//  ParticipatingFriendviewController.swift
+//  ManageUrTrip
+//
+//  Created by Marine GARDEISEN on 27/03/2019.
+//  Copyright © 2019 Marine GARDEISEN. All rights reserved.
+//
+
+import UIKit
+
+class ParticipatingFriendviewController: UIViewController {
+
+    
+    
+    @IBOutlet var ParticipantsViewController: TableParticipatingController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ TableParticipatingController.swift
@@ -0,0 +1,68 @@
+//
+//  TableParticipatingController.swift
+//  ManageUrTrip
+//
+//  Created by Marine GARDEISEN on 27/03/2019.
+//  Copyright © 2019 Marine GARDEISEN. All rights reserved.
+//
+
+import UIKit
+
+class TableParticipatingController: NSObject , UITableViewDataSource, FriendSetViewModelDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var friendSet_ViewModel : FriendSetViewModel
+    
+    override init() {
+        self.friendSet_ViewModel = FriendSetViewModel()
+        super.init()
+        self.friendSet_ViewModel.delegate=self
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        //permet de choisir le nombre de sections (confert fonction pour afficher le nom des section si on veut)
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //permet de choisir le nombre de lignes
+        return self.friendSet_ViewModel.count
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // indexPath = position de la cellule dans tableView
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellParticipants", for: indexPath)
+        guard let friend = self.friendSet_ViewModel.get(friendAt: indexPath.row) else{
+            fatalError("no travel found at this index")
+        }
+        cell.textLabel?.text = " \(friend.lastname) "
+        //cell.detailTextLabel?.text = " voici un sous titre"
+        //cell.imageView?.image = UIImage(named: "beach")
+        return cell
+    }
+    
+
+    func dataSetChanged() {
+        
+    }
+    
+    func friendDeleted(at indexPath: IndexPath) {
+        
+    }
+    
+    func friendUpdated(at indexPath: IndexPath) {
+        
+    }
+    
+    func friendAdded(at indexPath: IndexPath) {
+        
+    }
+    
+}
